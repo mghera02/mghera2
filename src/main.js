331beed9596a2a0fb7264a0ac9e1d04e8abf10aa
mghera02 <mghera02@hotmail.com>
2023-09-17 17:58:08 -0400

    Google analytics

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import VueGtag from 'vue-gtag'
+
+Vue.use(VueGtag, {
+    config: {
+        id: 'G-0RRKBDQ9B3',
+        params: {
+            send_page_view: false
+        }
+    }
+}, App)
 
 createApp(App).mount('#app')
