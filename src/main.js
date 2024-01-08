11f2daf62cdd267aa9bb5bd498e0e2aeb280bcf2
mghera02 <mghera02@hotmail.com>
2024-01-08 17:54:07 -0500

    reverting back SEO change

--- src/main.js
+++ src/main.js
@@ -1,18 +1,10 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import VueGtag from 'vue-gtag';
-import { createMetaManager } from 'vue-meta';
+import { createApp } from 'vue'
+import App from './App.vue'
+import VueGtag from 'vue-gtag'
+//import VueMeta from 'vue-meta';
 
-const app = createApp(App);
+//createApp(App).use(VueMeta);
 
-// Use VueGtag plugin
-app.use(VueGtag, {
-  config: { id: 'G-0RRKBDQ9B3' }
-});
-
-// Create a meta manager and assign it to the app's component option 'meta'
-const metaManager = createMetaManager();
-app.component('meta', metaManager);
-
-// Mount the app
-app.use(metaManager).mount('#app');
+createApp(App).use(VueGtag, {
+    config: {id: "G-0RRKBDQ9B3"}
+}).mount("#app")
