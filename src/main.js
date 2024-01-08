8fe4c1cd0225f7c08fdc08c9891fd7d1b74c85fb
mghera02 <mghera02@hotmail.com>
2024-01-08 17:51:02 -0500

    got rid of metadata for SEO due to bug

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import VueGtag from 'vue-gtag'
-import VueMeta from 'vue-meta';
+//import VueMeta from 'vue-meta';
 
-createApp(App).use(VueMeta);
+//createApp(App).use(VueMeta);
 
 createApp(App).use(VueGtag, {
     config: {id: "G-0RRKBDQ9B3"}
