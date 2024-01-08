47b44f04cbb0ab495a37bbd734b10f08468b19fd
mghera02 <mghera02@hotmail.com>
2024-01-08 17:49:11 -0500

    fixed bug with metadata for SEO

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import VueGtag from 'vue-gtag'
 import VueMeta from 'vue-meta';
 
-Vue.use(VueMeta);
+createApp(App).use(VueMeta);
 
 createApp(App).use(VueGtag, {
     config: {id: "G-0RRKBDQ9B3"}
