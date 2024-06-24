ea154d44828f55769d5cbcc5454928d9583eb999
mghera02 <mghera02@hotmail.com>
2024-06-24 17:33:16 -0400

    api bug fix for credentials pt2

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const AWS = require('aws-sdk');
 const bodyParser = require('body-parser');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const PORT = 8083;
 const { exec } = require('child_process');
